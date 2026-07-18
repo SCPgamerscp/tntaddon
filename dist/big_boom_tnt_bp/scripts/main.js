fe9fcd55204d2fd8ac189c689069d670e8c707e0
ailingmeng585-ship-it <ailingmeng585-ship-it@users.noreply.github.com>
2026-07-18 06:15:00 +0000

    fix(addon): repair packaging and TNT ignition

--- dist/big_boom_tnt_bp/scripts/main.js
+++ dist/big_boom_tnt_bp/scripts/main.js
@@ -1,28 +1,28 @@
 import { system } from "@minecraft/server";
 
+function ignite(block, dimension) {
+    dimension.playSound("fire.ignite", block.location);
+    dimension.spawnEntity("bigboomtnt:primed_big_boom_tnt", {
+        x: block.location.x + 0.5,
+        y: block.location.y,
+        z: block.location.z + 0.5
+    });
+    block.setType("minecraft:air");
+}
+
 const IgniteComponent = {
-    onPlayerInteract(e) {
-        const player = e.player;
-        const block = e.block;
-        const dimension = e.dimension;
-        
-        const equipment = player.getComponent("equippable");
-        if (equipment) {
-            const mainhand = equipment.getEquipment("Mainhand");
-            if (mainhand && mainhand.typeId === "minecraft:flint_and_steel") {
-                // Play ignition sound
-                dimension.playSound("fire.ignite", block.location);
-                
-                // Spawn primed TNT entity
-                dimension.spawnEntity("bigboomtnt:primed_big_boom_tnt", {
-                    x: block.location.x + 0.5,
-                    y: block.location.y,
-                    z: block.location.z + 0.5
-                });
-                
-                // Set the block to air
-                block.setType("minecraft:air");
-            }
+    onPlayerInteract(event) {
+        const equipment = event.player.getComponent("equippable");
+        const mainhand = equipment?.getEquipment("Mainhand");
+
+        if (mainhand?.typeId === "minecraft:flint_and_steel") {
+            ignite(event.block, event.dimension);
+        }
+    },
+
+    onTick(event) {
+        if ((event.block.getRedstonePower() ?? 0) > 0) {
+            ignite(event.block, event.dimension);
         }
     }
 };
